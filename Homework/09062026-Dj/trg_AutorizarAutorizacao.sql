ae76dcee8fa9a73b4349bc5de10dde7ee28ece5e
Djefferson dos Santos <djefferson141005@gmail.com>
2026-06-11 09:33:56 -0300

    fix: corrigindo indentação

--- Homework/09062026-Dj/trg_AutorizarAutorizacao.sql
+++ Homework/09062026-Dj/trg_AutorizarAutorizacao.sql
@@ -7,15 +7,15 @@ CREATE TRIGGER  [dbo].[trg_AutorizarAutorizacao]
 	ON [dbo].[Autorizacao]
 	AFTER INSERT
 	AS
-		/*	Documentação
+	/*	Documentação
 
-			Arquivo fonte:	trg_AtualizarSaldoFatura.sql
-			Objetivo:		Chamar a procedure [dbo].[sp_Autorizador] para verificar se uma nova instância na tabela
-							[dbo].[Autorizacao] cumpri os requisitos para ser validada. 
-			Autor:			Djefferson dos Santos Lima
-			Data Criação:	09/06/2026
-			
-		*/
+		Arquivo fonte:	trg_AtualizarSaldoFatura.sql
+		Objetivo:		Chamar a procedure [dbo].[sp_Autorizador] para verificar se uma nova instância na tabela
+						[dbo].[Autorizacao] cumpri os requisitos para ser validada. 
+		Autor:			Djefferson dos Santos Lima
+		Data Criação:	09/06/2026
+		
+	*/
 	BEGIN
 
 		DECLARE @IdNovaAutorizacao INT = ( SELECT Id FROM inserted );
--- Homework/09062026-Dj/trg_AutualizarSaldoSeAutorizacaoPermitida.sql
+++ Homework/09062026-Dj/trg_AutualizarSaldoSeAutorizacaoPermitida.sql
@@ -7,32 +7,32 @@ CREATE TRIGGER [dbo].[trg_AutualizarSaldoSeAutorizacaoPermitida]
 	ON [dbo].[Autorizacao]
 	AFTER UPDATE, DELETE
 	AS
-		/*	Documentação
-			
-			Arquivo fonte: trg_AutualizarSaldoSeAutorizacaoPermitida.sql
-			Objetivo:	Quando uma autorizacao for verificada ou deleteda, atualizar o saldo de acordo com as regras de
-						negócio.
-			Autor: Djefferson dos Santos Lima
-			Data criação: 10/06/2026
+	/*	Documentação
+		
+		Arquivo fonte: trg_AutualizarSaldoSeAutorizacaoPermitida.sql
+		Objetivo:	Quando uma autorizacao for verificada ou deleteda, atualizar o saldo de acordo com as regras de
+					negócio.
+		Autor: Djefferson dos Santos Lima
+		Data criação: 10/06/2026
 
-		*/
-		BEGIN
-			IF EXISTS ( SELECT 1 FROM deleted WHERE Autorizado = 1 )
-				BEGIN
-					UPDATE [dbo].[Fatura] 
-						SET Saldo = (fa.Saldo - d.Valor)
-						FROM [dbo].[Fatura] as fa
-							INNER JOIN deleted d
-								ON fa.Id = d.IdFatura
-									AND d.Autorizado = 1
-				END
+	*/
+	BEGIN
+		IF EXISTS ( SELECT 1 FROM deleted WHERE Autorizado = 1 )
+			BEGIN
+				UPDATE [dbo].[Fatura] 
+					SET Saldo = (fa.Saldo - d.Valor)
+					FROM [dbo].[Fatura] as fa
+						INNER JOIN deleted d
+							ON fa.Id = d.IdFatura
+								AND d.Autorizado = 1
+			END
 
-			IF EXISTS ( SELECT 1 FROM inserted WHERE Autorizado = 1 )
-				BEGIN
-					UPDATE [dbo].[Fatura] 
-						SET Saldo = (fa.Saldo + i.Valor)
-						FROM [dbo].[Fatura] as fa
-							INNER JOIN inserted i
-								ON fa.Id = i.IdFatura
-				END
-		END
+		IF EXISTS ( SELECT 1 FROM inserted WHERE Autorizado = 1 )
+			BEGIN
+				UPDATE [dbo].[Fatura] 
+					SET Saldo = (fa.Saldo + i.Valor)
+					FROM [dbo].[Fatura] as fa
+						INNER JOIN inserted i
+							ON fa.Id = i.IdFatura
+			END
+	END
